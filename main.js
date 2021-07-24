bbef3a0ca2c33d4cb0e4a14e8a4d34969f79622e
haps14 <79513622+haps14@users.noreply.github.com>
2021-07-24 15:01:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+song1 = "";
+song2 ="";
+song3 = "";
+song4 = "";
+function preload()
+{
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+    song3 = loadSound("music3.mp3");
+    song4 = loadSound("music4.mp3");
+}
+
+function setup()
+{
+
+
+}
+
+function draw()
+{
+
+
+}
+
+function play1()
+{
+    song1.play();
+
+}
+
+function play2()
+{
+    song2.play();
+}
+
+function play3()
+{
+    song3.play();
+}
+
+function play4()
+{
+    song4.play();
+}
+
+function stop1()
+{
+    song1.stop()
+}
+
+function stop2()
+{
+    song2.stop()
+}
+
+function stop3()
+{
+    song3.stop()
+}
+
+function stop4()
+{
+    song4.stop()
+}
